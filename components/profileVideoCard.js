166d3fe6d8e27b439bb1511aa286168fbf4d7f0b
Lukas Koenig <Lkoenig2121@gmail.com>
2024-07-28 15:49:46 -0400

    Added Profile Page

---
+++ components/profileVideoCard.js
@@ -0,0 +1,28 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import * as Icon from "react-native-feather";
+import { formatViews } from "../utils/numbers";
+export default function VideoCard({ video }) {
+  return (
+    <View className="h-64 w-60 pt-3">
+      <Image source={video && video.thumbnail} className="h-32 w-full" />
+      <View className="flex items-end mr-2 mb-5 -mt-6">
+        <View className="bg-black rounded px-1">
+          <Text className="text-white font-semibold text-xs">
+            {video && video.lengthText}
+          </Text>
+        </View>
+      </View>
+      <View className="flex-row justify-between items-center pb-5 space-x-3 mx-2">
+        <View className="flex-1 space-y-1">
+          <Text className="text-white font-semibold">
+            {video && video.title}
+          </Text>
+        </View>
+        <View className="self-start">
+          <Icon.MoreVertical stroke="white" strokeWidth={2} height={15} />
+        </View>
+      </View>
+    </View>
+  );
+}
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,11 +1,34 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  Button,
+} from "react-native";
+import VideoCard from "../components/profileVideoCard";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { fetchTrendingVideos } from "../api/youtube";
 import * as Icon from "react-native-feather";
 import { themeColors } from "../theme";
 import { useNavigation } from "@react-navigation/native";
 
 export default function ProfileScreen() {
+  const [activeButton, setActiveButton] = useState("All");
+  settings = ["Switch Account", "Google Account", "Turn on Incognito"];
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetchTrendingVideos();
+    setVideos(data);
+  };
+
   const navigation = useNavigation();
   const goToHome = () => {
     navigation.navigate("Home");
@@ -35,6 +58,83 @@ export default function ProfileScreen() {
           />
         </View>
       </SafeAreaView>
+      <View className="flex-row space-x-3">
+        <Image
+          source={require("../assets/images/avatar.jpg")}
+          className="h-20 w-20 rounded-full"
+        />
+
+        <View>
+          <Text className="text-white text-3xl">Lukas Koenig</Text>
+          <Text className="text-stone-400 text-xl">
+            @LukasKoenig - View Channel
+          </Text>
+        </View>
+      </View>
+
+      <View className="py-3 pb-3">
+        <ScrollView
+          className="px-4"
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        >
+          {settings.map((button, index) => {
+            let isActive = button == activeButton;
+            let textClass = isActive ? "text-black" : "text-white";
+            return (
+              <TouchableOpacity
+                // onPress={() => setActiveButton(button)}
+                key={index}
+                style={{
+                  backgroundColor: isActive ? "white" : "rgba(255,255,255,0.1)",
+                }}
+                className="rounded-md p-1 px-3 mr-2"
+              >
+                <Text className={textClass}>{button}</Text>
+              </TouchableOpacity>
+            );
+          })}
+        </ScrollView>
+      </View>
+
+      <View>
+        <Text className="text-white ml-5 mt-5 font-bold text-lg">HISTORY</Text>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          className="px-4"
+        >
+          {videos && videos[2] && <VideoCard video={videos[2]} />}
+          {videos && videos[4] && <VideoCard video={videos[4]} />}
+          {videos && videos[6] && <VideoCard video={videos[6]} />}
+        </ScrollView>
+      </View>
+
+      <View>
+        <TouchableOpacity className="flex-row">
+          <Image
+            source={require("../assets/images/youtube-image-better.avif")}
+            className="h-7 w-7 mt-3 ml-3 rounded-full"
+          />
+          <Text className="text-white ml-3 mt-5 font-bold">Your Videos</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View className="border-t-2 mt-5 border-gray-600">
+        <TouchableOpacity className="ml-3 mt-3">
+          <Text className="text-white mt-3 font-bold">Your movies & TV</Text>
+        </TouchableOpacity>
+        <TouchableOpacity className="ml-3 mt-3">
+          <Text className="text-white mt-3 font-bold">Get Youtube Premium</Text>
+        </TouchableOpacity>
+        <View className="border-t-2 mt-5 border-gray-600" />
+        <TouchableOpacity className="ml-3 mt-3">
+          <Text className="text-white mt-3 font-bold">Time Watched</Text>
+        </TouchableOpacity>
+        <TouchableOpacity className="ml-3 mt-3">
+          <Text className="text-white mt-3 font-bold">Help & Feedback</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
